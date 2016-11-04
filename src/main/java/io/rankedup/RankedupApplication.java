2f5f2bff65511a5a649dc6338cff6b23f1575af0
easigregory <fdsleeper@gmail.com>
2016-11-04 16:01:01 -0500

    Adding jax-rs and playing around with consuming APIs

--- src/main/java/io/rankedup/RankedupApplication.java
+++ src/main/java/io/rankedup/RankedupApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 @RestController
 @SpringBootApplication
 public class RankedupApplication {
@@ -23,8 +29,17 @@ public class RankedupApplication {
         return "Boot | Spring Boot";
     }
 
-    @RequestMapping("/test")
-    String test() {
-        return "Test | Spring Boot";
+    @RequestMapping("/auction")
+    String auction() {
+        Client client = ClientBuilder.newClient();
+        String server = "stormreaver";
+        String locale = "en_US";
+        String apiKey = "4b824btmudfvcp4k6xsz4eupp944kynk";
+
+        WebTarget target = client.target("https://us.api.battle.net/wow/auction/data").path(server).queryParam("locale", locale).queryParam("apiKey", apiKey);
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+        String responseEntity = response.readEntity(String.class);
+        return responseEntity;
     }
+
 }
